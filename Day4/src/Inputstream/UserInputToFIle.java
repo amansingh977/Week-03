c0a5ba3727eeaee8c1fdb4e0091daed41e65af81
amansingh977 <amansinghrajawat09@gmail.com>
2025-02-04 15:53:53 +0530

    Aman Add. UserInputToFIle.java

---
+++ Day4/src/Inputstream/UserInputToFIle.java
@@ -0,0 +1,30 @@
+package Inputstream;
+
+import java.io.*;
+
+public class UserInputToFIle {
+    public static void main(String[] args) {
+        String filePath = "E:\\Capg\\Week3\\Day4\\src\\filereader\\file.txt";
+
+        try (
+                InputStreamReader isr = new InputStreamReader(System.in);
+                BufferedReader br = new BufferedReader(isr);
+                FileWriter fw = new FileWriter(filePath, true); // Append mode
+                BufferedWriter bw = new BufferedWriter(fw)) {
+
+            System.out.println("Enter text to write to file (type 'exit' to stop):");
+
+            String input;
+            while (!(input = br.readLine()).equalsIgnoreCase("exit")) {
+                bw.write(input);
+                bw.newLine(); // Write each input as a new line
+            }
+
+            System.out.println("User input has been saved to " + filePath);
+
+        } catch (
+                IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+    }
+}
